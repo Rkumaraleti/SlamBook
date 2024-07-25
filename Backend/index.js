f3bf6e2c0a440dc23fe250eadeb4bd2bf621432f
Rkumaraleti <renukakumar9030@gmail.com>
2024-07-25 18:35:46 +0530

    Establishing Mongo and Routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const dotenv = require('dotenv');
 
+//Express Config:
+const express = require('express');
 const app = express();
+
+//Env File Configuration:
+const dotenv = require('dotenv');
 dotenv.config({path: __dirname+'/../.env'});
+
+// Mongoose & Mongo:
+const mongoose = require('mongoose');
+const mongo_url = process.env.MONGO_URL;
+
+// Mongoose Connect:
+mongoose.connect(mongo_url);
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+//Routes: 
+const homeRoutes = require('./routes/homeRoutes')
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Home")
-})
+app.use('/', homeRoutes);
 
 app.listen(PORT, () => {
     console.log("Backend is Active")
